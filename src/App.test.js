e61ed1ae76c16096d850c4b979ab416b418ba443
Viktoriya Myalo <viktoriya.myalo.lystopad@gmail.com>
2018-05-14 17:35:36 +0300

    Minor styling fixes

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { configure, shallow, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import toJson from 'enzyme-to-json';
+
+configure({ adapter: new Adapter() });
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+describe("<App />", () => {
+  const wrapper = shallow(<App />);
+  it("should contain one div element", () => {
+    expect (wrapper.find('div').length).toBe(1);
+  })
+})
